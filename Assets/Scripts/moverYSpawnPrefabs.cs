bb6080edb95a427b68f93c37d747db0edcc66758
LucaFedericoMarty <101400526+LucaFedericoMarty@users.noreply.github.com>
2022-05-23 21:26:19 -0300

    Vector3 HECHO

---
+++ Assets/Scripts/moverYSpawnPrefabs.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moverYSpawnPrefabs : MonoBehaviour
+{
+    public float xSpeed;
+    public float rectanguloMaxPosicionDerecha;
+    public float rectanguloMaxPosicionIzquierda;
+    public bool toRight;
+
+    public GameObject sphereSpawn;
+    public GameObject cylinderSpawn;
+
+    public GameObject BaseIZQ;
+    public GameObject BaseDER;
+
+    int altura;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        altura = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        GameObject clon;
+
+        if (transform.position.x >= rectanguloMaxPosicionDerecha)
+        {
+            
+            toRight = false;
+            clon = Instantiate(sphereSpawn);
+            clon.transform.position = BaseDER.transform.position + new Vector3 (0,altura,0);
+            altura++;
+        }
+
+        else if (transform.position.x <= rectanguloMaxPosicionIzquierda)
+        {
+            
+            toRight = true;
+            clon = Instantiate(cylinderSpawn);
+            clon.transform.position = BaseIZQ.transform.position + new Vector3(0, altura, 0);
+            
+        }
+
+
+        if (toRight)
+        {
+            transform.position += new Vector3(xSpeed, 0, 0);
+        }
+
+        else
+        {
+            transform.position -= new Vector3(xSpeed, 0, 0);
+        }
+
+    }
+}
---
+++ Assets/Scripts/rectanguloMovedizoBehaviour.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rectanguloMovedizoBehaviour : MonoBehaviour
+{
+    public float xSpeed;
+    public float rectanguloMaxPosicionDerecha;
+    public float rectanguloMaxPosicionIzquierda;
+    public bool toRight;
+    public GameObject sphereSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        toRight = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (transform.position.x >= rectanguloMaxPosicionDerecha)
+        {
+            toRight = false;
+            Instantiate(sphereSpawn);
+            
+        }
+
+        else if (transform.position.x <= rectanguloMaxPosicionIzquierda)
+        {
+            toRight = true;
+            Instantiate(sphereSpawn);
+        }
+
+
+        if (toRight)
+        {
+            transform.position += new Vector3(xSpeed, 0, 0);
+        }
+
+        else
+        {
+            transform.position -= new Vector3(xSpeed, 0, 0);
+        }
+
+        ////if (toRight)
+        //{
+        //    if (transform.position.x <= rectanguloMaxPosicionDerecha)
+        //    {
+        //        transform.position += new Vector3(xSpeed, 0, 0);
+
+        //        if (transform.position.x == rectanguloMaxPosicionDerecha)
+        //        {
+        //            toRight = false;
+        //        }
+        //    }
+
+        //}
+
+        //else if (toRight == false)
+        //{
+        //    transform.position += new Vector3(-xSpeed, 0, 0);
+        //}
+
+    }
+}
